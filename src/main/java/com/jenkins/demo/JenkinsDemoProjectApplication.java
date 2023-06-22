21be9a4c6b2777cb1eb2752e476548ac17652005
unknown <vaibhav8976kalgude@gmail.com>
2023-06-22 04:43:31 +0200

    Jenkins commit 2

--- src/main/java/com/jenkins/demo/JenkinsDemoProjectApplication.java
+++ src/main/java/com/jenkins/demo/JenkinsDemoProjectApplication.java
@@ -1,10 +1,11 @@
 package com.jenkins.demo;
 
-import jakarta.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class JenkinsDemoProjectApplication {
